ea82dd89257e3df0d7c7524ebf58203b614e0099
David González <dav.gonzalezortiz@gmail.com>
2025-05-25 19:24:08 +0200

    Primer commit - subida inicial del proyecto

---
+++ MiAppMvc/Program.cs
@@ -0,0 +1,45 @@
+using MiAppMvc.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Configurar el DbContext con la cadena de conexión del archivo settings.json
+builder.Services.AddDbContext<DbContextEventos>(options =>
+   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Home/InicioSesion"; // ruta a la vista de login
+        options.AccessDeniedPath = "/Home/AccesoDenegado"; // si usas restricciones
+    });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
